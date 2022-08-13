b1584e0d7c622f0fea86e3735fd6f1688b765927
Fernando Galvan <n.brighter00@gmail.com>
2022-08-13 17:52:23 -0500

    adding simple pages

--- app.py
+++ app.py
@@ -3,5 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hell_world():
-    return "<p> Hello World <p>"
+def index():
+    return "Hello!"
+
+@app.route('/about')
+def about():
+    return "About"
+
+if __name__ == "__main__":
+    app.run(debug=True)
