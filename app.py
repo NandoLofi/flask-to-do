e077f74b2ea0df76f1ec05427fc47d6a020cac95
Fernando Galvan <n.brighter00@gmail.com>
2022-08-14 23:19:29 -0500

    adding database and class

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+import os
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = '/Users/fgalvan/Library/ApplicationSupport/Postgres/var-14/postgresql.conf'
+
+db = SQLAlchemy(app)
+
+class To_Do(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
 
 @app.route('/')
 def index():
